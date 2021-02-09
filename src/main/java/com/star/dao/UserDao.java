1c99d411d508237467b557d647a31ddf3015fac4
xushuanglong <1872744675@qq.com>
2021-02-09 21:28:45 +0800

    build

---
+++ src/main/java/com/star/dao/UserDao.java
@@ -0,0 +1,16 @@
+package com.star.dao;
+
+import com.star.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @Description: 用户持久层接口
+ * @Author: Goodenough
+ */
+@Mapper
+@Repository
+public interface UserDao {
+    User findByUsernameAndPassword(@Param("username") String username,@Param("password") String password);
+}
